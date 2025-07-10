687057cb9dbda3124768d314cd4f6e5cd203e645
acandoo <117209328+acandoo@users.noreply.github.com>
2025-07-10 13:10:51 -0400

    Test

---
+++ index.js
@@ -0,0 +1,4 @@
+import git from 'isomorphic-git';
+import http from 'isomorphic-git/http/node';
+console.log(git.version());
+http;
---
+++ index.ts
@@ -0,0 +1,5 @@
+import git from 'isomorphic-git'
+import http from 'isomorphic-git/http/node'
+
+console.log(git.version())
+http;
